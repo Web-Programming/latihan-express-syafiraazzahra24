0895e0ec63764dc58b77f396b3e807f92a53d9a5
syafiraazzahra24 <160854047+syafiraazzahra24@users.noreply.github.com>
2024-09-27 16:59:27 +0700

    latihan express syafira
    latihan express

---
+++ mynodeapp/app.js
@@ -0,0 +1,25 @@
+
+//app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//route get
+//app.get('/', (req,res) => {
+    //res.send('hello, GET request!.');
+//});
+
+//middleware untuk parsing body req
+app.use(express.json());
+
+//route post
+app.post('/submit',(req,res)=> {
+    const {name} = req.body;
+    res.send('hello, ${name}!');
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log('server running at http://localhost:${port}/');
+});
