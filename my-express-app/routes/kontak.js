779438533dac3f4abe9ad4c1381ac38578cd0751
syafiraazzahra24 <160854047+syafiraazzahra24@users.noreply.github.com>
2024-10-02 09:31:07 +0700

    belajar express
    latihan belajar membuat express halman utama, kontak, about us

---
+++ my-express-app/routes/kontak.js
@@ -0,0 +1,4 @@
+app.get('/kontak', (req, res) => {
+    res.render('kontak'); // Render view kontak.ejs
+});
+
--- mynodeapp/app.js
+++ mynodeapp/app.js
@@ -6,15 +6,15 @@ const port = 3000;
 
 //route get
 //app.get('/', (req,res) => {
-    //res.send('hello, GET request!.');
+//res.send('hello, GET request!.');
 //});
 
 //middleware untuk parsing body req
 app.use(express.json());
 
 //route post
-app.post('/submit',(req,res)=> {
-    const {name} = req.body;
+app.post('/submit', (req, res) => {
+    const { name } = req.body;
     res.send('hello, ${name}!');
 });
 
@@ -23,3 +23,6 @@ app.use(express.static('public'));
 app.listen(port, () => {
     console.log('server running at http://localhost:${port}/');
 });
+
+
+
