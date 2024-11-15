fd75852917acc6a860d92cd003b23043731a09c5
syafiraazzahra24 <marselynafitrimawarni@gmail.com>
2024-11-15 16:51:06 +0700

    komponen

--- syafira-app/src/app/housing-location/housing-location.component.ts
+++ syafira-app/src/app/housing-location/housing-location.component.ts
@@ -9,9 +9,12 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule, CommonModule],
   template: `
       <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <img class="listing-photo" [src]="housingLocation.photo" 
+      alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <p class="listing-location">{{ housingLocation.city}},
+       {{housingLocation.state }}</p>
+      <p style"padding: 0 0 10px 20px;">{{ housingLocation.availabeUnits}} Unit(s)</p>
       <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
